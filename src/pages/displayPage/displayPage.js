efc760570eb4bdf098f003d718290b9e8ac17047
awwal <leyeawwal@gmail.com>
2024-08-23 17:05:36 +0100

    added framer motion

--- src/pages/displayPage/displayPage.js
+++ src/pages/displayPage/displayPage.js
@@ -4,20 +4,39 @@ import Home from "../home/home";
 import Middle from "../middle/middle";
 import AllProductsHome from "../allProductsHome/allProductsHome";
 import AllHomeCategory from "../allProductsHome/allHomeCategory";
+import { motion } from "framer-motion";
 
 const DisplayPage = () => {
+	const sliderVariants = {
+		initial: {
+			x: "100%",
+		},
+		animate: {
+			x: "-100%",
+			transition: {
+				repeat: Infinity,
+				repeatType: "loop",
+				duration: 1,
+				ease: "linear", 
+			},
+		},
+	};
+
 	return (
 		<div>
 			<MetaData title="Zarmario Shop" />
-
 			<div className="displayPage">
 				<Home />
 				<AllProductsHome />
 				<div className="marioText">
-					<div>
-						ZARMARIO!!! HOME OF EXCLUSIVE FASHIONABLE WEARS - WHERE MARIO THERE'S
-						DRIP - WEAR MARIO THERE'S LIGHT.
-					</div>
+					<motion.div
+						variants={sliderVariants}
+						initial="initial"
+						animate="animate"
+					>
+						<p>ZARMARIO!!! HOME OF EXCLUSIVE FASHIONABLE WEARS - WHERE MARIO THERE'S DRIP - WEAR MARIO THERE'S LIGHT.</p>
+					</motion.div>
+					<div className="mTB"></div>
 				</div>
 				<div className="allHomeCategoryFile">
 					<AllHomeCategory />
