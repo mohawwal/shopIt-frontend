064083889d8b63b9b357ea3e5e7fec60aa082bb6
awwal <leyeawwal@gmail.com>
2024-08-26 13:00:54 +0100

    login user

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,33 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import "./user.css";
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import ClipLoader from "react-spinners/ClipLoader";
 import { login, clearErrors } from "../../actions/userAction";
 import googleIcon from "../../assets/images/google_icon.png";
-//import AlertContext from "../alert/AlertContext";
+import AlertContext from "../alert/AlertContext";
 
 const Login = () => {
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
-	//const alert = useAlert();
 
-	// const [, setAlert] = useContext(AlertContext);
+	 const [, setAlert] = useContext(AlertContext);
 
-	// const showAlert = (message, type) => {
-	// 	setAlert({
-	// 		message,
-	// 		type,
-	// 	});
-	// };
+	 const showAlert = (message, type) => {
+	 	setAlert({
+	 		message,
+	 		type,
+	 	});
+	 };
 
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-	const [formSubmitted, setFormSubmitted] = useState(false)
 
 	const submitHandler = (e) => {
 		e.preventDefault();
-		setFormSubmitted(true)
 		dispatch(login(email, password));
 	};
 
@@ -36,16 +33,16 @@ const Login = () => {
 	);
 
 	useEffect(() => {
-		if (formSubmitted && isAuthenticated) {
+		if (isAuthenticated) {
 			navigate("/");
-			//showAlert("Logged In successful", "success");
+			showAlert("Logged In successful", "success");
 		}
 
-		if (formSubmitted && error) {
-			//showAlert(error, "error");
+		if (error) {
+			showAlert(error, "error");
 			dispatch(clearErrors());
 		}
-	}, [dispatch, error, navigate, formSubmitted]);
+	}, [dispatch, error, navigate]);
 
 	// if(isAuthenticated) {
 	// 	return null
